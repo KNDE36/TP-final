4feea8926faefd75c82e85b63295448e577e7dae
knde36 <cannmartin1@gmail.com>
2023-11-22 23:38:03 -0300

    lalal

--- tp_final.R
+++ tp_final.R
@@ -4,12 +4,6 @@ require(metR)
 require(ggplot2)
 #cargo las librerias necesarias
 
-GlanceNetCDF(archivo)
-nc<-nc_open(archivo)
-nc
-datos_sst<-ReadNetCDF(archivo,vars = "sst")
-#no es necesario abrirlo completo, de una ya completado esta ok
-
 
 ############ ITEM A ###############
 
@@ -19,7 +13,7 @@ archivo<-"C:/Users/cannm/OneDrive/Documentos/sst.mnmean_ERSST.nc" #CASA
 archivo<-"~/Downloads/sst.mnmean_ERSST.nc"  #COMPU LUNES
 GlanceNetCDF(archivo)
 
-#abro para seleccionando la region mar argentino y el periodo de 1990-2019 (30 años) 
+#abro para seleccionando la region mar argentino y el periodo de 1990-2019 (30 a?os) 
 sst_mar_argentino<-ReadNetCDF(archivo,vars = "sst",subset = list(lat=c(-60,-20),lon=c(290,320),time=c("1990-01-01","2019-12-01")))
 
 #arreglo las longitudes para que tome bien el formato (-180 a 180)
@@ -29,17 +23,18 @@ sst_mar_argentino$lon<-ConvertLongitude(sst_mar_argentino$lon)
 mapa<-map_data("world")
 
 mi_mapa<-geom_path(data=mapa,mapping=aes(long,lat,group=group))
+
 ggplot(sst_mar_argentino,aes(x=lon,y=lat))+
   geom_raster(aes(fill=sst))+
   mi_mapa+
-  coord_sf(xlim=c(-180,20),ylim=c(-90,0))+
+  coord_sf(xlim=c(-80,-20),ylim=c(-65,0))+
   geom_rect(xmin=-70,xmax=-40,ymin=-60,ymax=-20,fill=NA,colour="red")+
   theme(panel.background = element_rect(fill="skyblue"),
         panel.ontop = F)+
   labs(x="Longitud",
        y="Latitud",
-       fill="Temp superficial del mar (C°)")
-  
+       fill="Temp superficial del mar (CÂ°)")
+ ###CORREGIR QUE SOLO ESTE PINTADO EL OCEANO Y NO LA SUP TERRESTRE!! 
 
 
 
@@ -57,22 +52,24 @@ climatologia<-aggregate(sst_mar_argentino$sst,list(sst_mar_argentino$mes,sst_mar
 colnames(climatologia)<-c("Mes","Latitud","Longitud","Climatologia.Mensual")
 #nombro a las columnas con las variables
 
+climatologia$Longitud<-ConvertLongitude(climatologia$Longitud)
 
 
-#opcion 1  graficar en el ciclo y guardarlo , ES LA IDEA
+
 meses<-1:12
 for (i in meses) {
   mes<-meses[i]
-  #datos_clima_mes<-climatologia[climatologia$Mes==mes,]
-  g<-ggplot(climatologia,aes(x=Longitud,y=Latitud))+
+  campos<-ggplot(climatologia,aes(x=Longitud,y=Latitud))+
     geom_raster(aes(fill=Climatologia.Mensual))+
-    coord_sf(xlim=c(360-80,360-20),ylim=c(-70,-20))+
+    mi_mapa+
+    coord_sf(xlim=c(-80,-20),ylim=c(-70,-20))+
     facet_wrap(~Mes,ncol=6)+
     labs(x="Longitud",
          y="Latitud",
          fill="Climatologia Mensual")
-  }
+}
 
+campos #para ver los campos graficados
 
 
 
@@ -94,7 +91,7 @@ sst_30S$mes<-month(sst_30S$time)
 sst_30S$anio<-year(sst_30S$time)
 
 
-#promedio para 40°S y otro para 30°S
+#promedio para 40?S y otro para 30?S
 serie_temp_40s<-aggregate(sst_40S$sst,list(sst_40S$mes,sst_40S$lat,sst_40S$anio),mean)
 serie_temp_30s<-aggregate(sst_30S$sst,list(sst_30S$mes,sst_30S$lat,sst_30S$anio),mean)
 
@@ -107,7 +104,7 @@ serie_temp_30s$Fecha<-paste(serie_temp_30s$Anio,serie_temp_30s$Mes,sep = "-")
 ggplot(serie_temp_30s,mapping=aes(x=Fecha,y=Promedio))+
   geom_line(color="red")+
   labs(title="Serie Temporal Promedio",
-       subtitle = "Latitud: 30°S",
+       subtitle = "Latitud: 30Â°S",
        fill="Anio")+
   theme_linedraw()
 
@@ -116,8 +113,8 @@ ggplot(serie_temp_30s,mapping=aes(x=Fecha,y=Promedio))+
 ggplot(serie_temp_40s,aes(x=Mes,y=Promedio,color=Anio))+
   geom_col(aes(fill=factor(Anio)),position = "dodge")+
   scale_color_continuous(aes("Anios"))+
-  labs(title="Serie Temporal Promedio de la latitud 40°S",
-       subtitle = "Latitud: 40°S ",
+  labs(title="Serie Temporal Promedio de la latitud 40Â°S",
+       subtitle = "Latitud: 40Â°S ",
        x="Meses",
        fill="Anio")+
   theme_get()
@@ -127,7 +124,7 @@ ggplot(serie_temp_40s,aes(x=Mes,y=Promedio,color=Anio))+
 ############# ITEM D #############
 
 
-#promedio para 40°S y otro para 30°S
+#promedio para 40?S y otro para 30?S
 onda_anual_40s<-aggregate(serie_temp_40s$Promedio,list(serie_temp_40s$Mes,serie_temp_40s$Latitud),mean)
 onda_anual_30s<-aggregate(serie_temp_30s$Promedio,list(serie_temp_30s$Mes,serie_temp_30s$Latitud),mean)
 
@@ -148,10 +145,10 @@ ggplot(onda_anual_30s,aes(x=Mes,y=(Promedio)))+
   geom_line(color="cornflowerblue")+
   geom_point(color="midnightblue",size=4,alpha=0.6)+
   labs(title="Onda Anual",
-       subtitle = "Latitud: 30°S
+       subtitle = "Latitud: 30?S
 Promedio de temperatura",
        x="Meses",
-       y="Temperatura(C°)")+
+       y="Temperatura(CÂ°)")+
   theme_get()+
   scale_x_continuous(breaks = c(1:12))
 
@@ -162,10 +159,10 @@ ggplot(onda_anual_40s,aes(x=Mes,y=Promedio))+
   geom_point(size=4,alpha=0.6)+
   scale_color_continuous()+
   labs(title="Onda Anual",
-       subtitle = "Latitud: 40°S
+       subtitle = "Latitud: 40Â°S
 Promedio de Temperatura",
        x="Meses",
-       y="Temperatura (C°)")+
+       y="Temperatura (CÂ°)")+
   theme_get()+
   scale_x_continuous(breaks = c(1:12))
 
@@ -196,7 +193,7 @@ serie_temp_30s$nro=1:length(serie_temp_30s$Fecha)
 g<-ggplot(data=serie_temp_30s,aes(x=nro,y=Promedio,color=Promedio))+
   geom_line(aes(color="Promedio"))+
   labs(title="Serie Temporal Promedio",
-       subtitle = "Latitud: 30°S",
+       subtitle = "Latitud: 30?S",
        x="Meses",
        fill="Anio")+xlim(0,2030)+ylim(min(serie_temp_30s$Promedio),max(serie_temp_30s$Promedio))+theme_linedraw()+ theme(legend.position = "none")
 g<-g+ scale_x_continuous(labels=serie_temp_30s$Fecha[seq(0, 2030, 50)],breaks = seq(0, 2030, 50))
